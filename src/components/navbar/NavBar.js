abb9180d83fddde0e89c14e37140cf25d61d1cb9
Gabo <gabrielathie@hotmail.com>
2025-09-14 23:21:14 -0600

    Navbar Fix for mobiles

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -6,7 +6,7 @@ import { NavLink } from 'react-router-dom';
 
 function NavBar() {
     return (
-        <Navbar className="romantic-navbar" expand="lg">
+        <Navbar className="romantic-navbar" expand="lg" collapseOnSelect>
             <Container className="romantic-container justify-content-center">
                 <Navbar.Brand as={NavLink} to="/" className="romantic-brand">
                     Karen No Uta
@@ -14,12 +14,30 @@ function NavBar() {
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="romantic-nav ms-auto">
-                        <Nav.Link as={NavLink} to="/chapter1" className="nav-link">Chapter 1</Nav.Link>
-                        <Nav.Link as={NavLink} to="/chapter2" className="nav-link">Chapter 2</Nav.Link>
-                        <Nav.Link as={NavLink} to="/chapter3" className="nav-link">Chapter 3</Nav.Link>
-                        <Nav.Link as={NavLink} to="/chapter4" className="nav-link">Chapter 4</Nav.Link>
-                        <Nav.Link as={NavLink} to="/gallery" className="nav-link">Gallery</Nav.Link>
-                        <Nav.Link as={NavLink} to="/decisions" className="nav-link">Decisions</Nav.Link>
+                        <Nav.Link as={NavLink} to="/chapter1" className="nav-link">
+                            <span className="desktop-text">Chapter 1</span>
+                            <span className="mobile-text">C1</span>
+                        </Nav.Link>
+                        <Nav.Link as={NavLink} to="/chapter2" className="nav-link">
+                            <span className="desktop-text">Chapter 2</span>
+                            <span className="mobile-text">C2</span>
+                        </Nav.Link>
+                        <Nav.Link as={NavLink} to="/chapter3" className="nav-link">
+                            <span className="desktop-text">Chapter 3</span>
+                            <span className="mobile-text">C3</span>
+                        </Nav.Link>
+                        <Nav.Link as={NavLink} to="/chapter4" className="nav-link">
+                            <span className="desktop-text">Chapter 4</span>
+                            <span className="mobile-text">C4</span>
+                        </Nav.Link>
+                        <Nav.Link as={NavLink} to="/gallery" className="nav-link">
+                            <span className="desktop-text">Gallery</span>
+                            <span className="mobile-text">Gallery</span>
+                        </Nav.Link>
+                        <Nav.Link as={NavLink} to="/decisions" className="nav-link">
+                            <span className="desktop-text">Decisions</span>
+                            <span className="mobile-text">Decisions</span>
+                        </Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
